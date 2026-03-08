57adf1a8daf496daabff3f5d965a6c1b3740740e
Nanda Perbawa <nandaperbawa@gmail.com>
2026-03-08 09:29:43 +0700

    Handle submit buat nyambungin backend

--- Backend/bootstrap/app.php
+++ Backend/bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->prepend([
+            \Illuminate\Http\Middleware\HandleCors::class, // Ini buat ngatasin masalah CORS
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
